b78e630c8976bba3ae1149232a2ba6d5d282c482
Edgar Robles <tgo.edrobles@gmail.com>
2023-07-02 18:52:55 -0600

    Se agregan archivo para ignorar seguimiento y se comenta funciones no utilizadas

--- main/espHomeMqtt5.c
+++ main/espHomeMqtt5.c
@@ -37,7 +37,7 @@
 #define ESP_INTR_FLAG_DEFAULT 0
 #define USE_PROPERTY_ARR_SIZE   sizeof(user_property_arr)/sizeof(esp_mqtt5_user_property_item_t)
 static const char *TAG = "MQTT5_EXAMPLE";
-
+bool isconnected=false;
 static QueueHandle_t gpio_evt_queue = NULL;
 
 static void IRAM_ATTR gpio_isr_handler(void* arg)
@@ -49,18 +49,29 @@ static void IRAM_ATTR gpio_isr_handler(void* arg)
 static void gpio_task_example(void* arg)
 {
 	uint32_t io_num;
-	for(;;) {
-		if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+	for(;;) 
+	{
+		if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)&&isconnected) 
+		{
 			printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
 		}
 	}
 }
 
+static esp_mqtt5_publish_property_config_t publish_property = {
+	.payload_format_indicator = 1,
+	.message_expiry_interval = 1000,
+	.topic_alias = 0,
+	.response_topic = "/topic/test/response",
+	.correlation_data = "123456",
+	.correlation_data_len = 6,
+};
+
 static esp_mqtt5_user_property_item_t user_property_arr[] = {
         {"board", "esp32"},
         {"u", "mqtt_user"},
         {"p", "mqttpass"}
-    };
+};
 static void mqtt5_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
     ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32, base, event_id);
@@ -72,79 +83,80 @@ static void mqtt5_event_handler(void *handler_args, esp_event_base_t base, int32
     switch ((esp_mqtt_event_id_t)event_id) {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        print_user_property(event->property->user_property);
-        esp_mqtt5_client_set_user_property(&publish_property.user_property, user_property_arr, USE_PROPERTY_ARR_SIZE);
-        esp_mqtt5_client_set_publish_property(client, &publish_property);
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 1);
-        esp_mqtt5_client_delete_user_property(publish_property.user_property);
-        publish_property.user_property = NULL;
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+	isconnected=true;
+	esp_mqtt5_client_set_user_property(&publish_property.user_property, user_property_arr, USE_PROPERTY_ARR_SIZE);
+	esp_mqtt5_client_set_publish_property(client, &publish_property);
+	msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 1);
+	esp_mqtt5_client_delete_user_property(publish_property.user_property);
+	publish_property.user_property = NULL;
+	ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        //print_user_property(event->property->user_property);
 
-        esp_mqtt5_client_set_user_property(&subscribe_property.user_property, user_property_arr, USE_PROPERTY_ARR_SIZE);
-        esp_mqtt5_client_set_subscribe_property(client, &subscribe_property);
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-        esp_mqtt5_client_delete_user_property(subscribe_property.user_property);
-        subscribe_property.user_property = NULL;
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+        //esp_mqtt5_client_set_user_property(&subscribe_property.user_property, user_property_arr, USE_PROPERTY_ARR_SIZE);
+        //esp_mqtt5_client_set_subscribe_property(client, &subscribe_property);
+        //msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+        //esp_mqtt5_client_delete_user_property(subscribe_property.user_property);
+        //subscribe_property.user_property = NULL;
+        //ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
-        esp_mqtt5_client_set_user_property(&subscribe1_property.user_property, user_property_arr, USE_PROPERTY_ARR_SIZE);
-        esp_mqtt5_client_set_subscribe_property(client, &subscribe1_property);
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 2);
-        esp_mqtt5_client_delete_user_property(subscribe1_property.user_property);
-        subscribe1_property.user_property = NULL;
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+        //esp_mqtt5_client_set_user_property(&subscribe1_property.user_property, user_property_arr, USE_PROPERTY_ARR_SIZE);
+        //esp_mqtt5_client_set_subscribe_property(client, &subscribe1_property);
+        //msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 2);
+        //esp_mqtt5_client_delete_user_property(subscribe1_property.user_property);
+        //subscribe1_property.user_property = NULL;
+        //ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
-        esp_mqtt5_client_set_user_property(&unsubscribe_property.user_property, user_property_arr, USE_PROPERTY_ARR_SIZE);
-        esp_mqtt5_client_set_unsubscribe_property(client, &unsubscribe_property);
-        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos0");
-        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
-        esp_mqtt5_client_delete_user_property(unsubscribe_property.user_property);
-        unsubscribe_property.user_property = NULL;
+        //esp_mqtt5_client_set_user_property(&unsubscribe_property.user_property, user_property_arr, USE_PROPERTY_ARR_SIZE);
+        //esp_mqtt5_client_set_unsubscribe_property(client, &unsubscribe_property);
+        //msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos0");
+        //ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+        //esp_mqtt5_client_delete_user_property(unsubscribe_property.user_property);
+        //unsubscribe_property.user_property = NULL;
         break;
     case MQTT_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-        print_user_property(event->property->user_property);
+        //ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        //print_user_property(event->property->user_property);
         break;
     case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        print_user_property(event->property->user_property);
-        esp_mqtt5_client_set_publish_property(client, &publish_property);
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        //ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        //print_user_property(event->property->user_property);
+        //esp_mqtt5_client_set_publish_property(client, &publish_property);
+        //msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+        //ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
         break;
     case MQTT_EVENT_UNSUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-        print_user_property(event->property->user_property);
-        esp_mqtt5_client_set_user_property(&disconnect_property.user_property, user_property_arr, USE_PROPERTY_ARR_SIZE);
-        esp_mqtt5_client_set_disconnect_property(client, &disconnect_property);
-        esp_mqtt5_client_delete_user_property(disconnect_property.user_property);
-        disconnect_property.user_property = NULL;
-        esp_mqtt_client_disconnect(client);
+        //ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        //print_user_property(event->property->user_property);
+        //esp_mqtt5_client_set_user_property(&disconnect_property.user_property, user_property_arr, USE_PROPERTY_ARR_SIZE);
+        //esp_mqtt5_client_set_disconnect_property(client, &disconnect_property);
+        //esp_mqtt5_client_delete_user_property(disconnect_property.user_property);
+        //disconnect_property.user_property = NULL;
+        //esp_mqtt_client_disconnect(client);
         break;
     case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-        print_user_property(event->property->user_property);
+        //ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        //print_user_property(event->property->user_property);
         break;
     case MQTT_EVENT_DATA:
-        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        print_user_property(event->property->user_property);
-        ESP_LOGI(TAG, "payload_format_indicator is %d", event->property->payload_format_indicator);
-        ESP_LOGI(TAG, "response_topic is %.*s", event->property->response_topic_len, event->property->response_topic);
-        ESP_LOGI(TAG, "correlation_data is %.*s", event->property->correlation_data_len, event->property->correlation_data);
-        ESP_LOGI(TAG, "content_type is %.*s", event->property->content_type_len, event->property->content_type);
-        ESP_LOGI(TAG, "TOPIC=%.*s", event->topic_len, event->topic);
-        ESP_LOGI(TAG, "DATA=%.*s", event->data_len, event->data);
+        //ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        //print_user_property(event->property->user_property);
+        //ESP_LOGI(TAG, "payload_format_indicator is %d", event->property->payload_format_indicator);
+        //ESP_LOGI(TAG, "response_topic is %.*s", event->property->response_topic_len, event->property->response_topic);
+        //ESP_LOGI(TAG, "correlation_data is %.*s", event->property->correlation_data_len, event->property->correlation_data);
+        //ESP_LOGI(TAG, "content_type is %.*s", event->property->content_type_len, event->property->content_type);
+        //ESP_LOGI(TAG, "TOPIC=%.*s", event->topic_len, event->topic);
+        //ESP_LOGI(TAG, "DATA=%.*s", event->data_len, event->data);
         break;
     case MQTT_EVENT_ERROR:
-        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-        print_user_property(event->property->user_property);
-        ESP_LOGI(TAG, "MQTT5 return code is %d", event->error_handle->connect_return_code);
-        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
-            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
-            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
-            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
-            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
-        }
+        //ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        //print_user_property(event->property->user_property);
+        //ESP_LOGI(TAG, "MQTT5 return code is %d", event->error_handle->connect_return_code);
+        //if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+        //    log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+        //    log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+        //    log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+        //    ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+        //}
         break;
     default:
         ESP_LOGI(TAG, "Other event id:%d", event->event_id);
@@ -256,9 +268,9 @@ void app_main(void)
 	//hook isr handler for specific gpio pin
 	gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
 	//remove isr handler for gpio number.
-	gpio_isr_handler_remove(GPIO_INPUT_IO_0);
+	//gpio_isr_handler_remove(GPIO_INPUT_IO_0);
 	//hook isr handler for specific gpio pin again
-	gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+	//gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
 
     int cnt = 0;
     while(1) {
@@ -267,4 +279,5 @@ void app_main(void)
         gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
         gpio_set_level(GPIO_OUTPUT_IO_1, cnt % 2);
     }
+    mqtt5_app_start();
 }
